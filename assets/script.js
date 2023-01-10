129b7aff88f5e5a4a716a0a7b14e7cacb6367423
John Hornick <jhornick@live.com>
2023-01-10 16:55:48 -0500

    Data put on page.

--- assets/script.js
+++ assets/script.js
@@ -18,8 +18,37 @@ fetch(apiURL)
 })
 .then(function (data) {
     //display results
+    //TODO change selector to get parent div
+    var displayArea = document.getElementById('outputarea');
     for(var i = 0; i < data.results.length; i++) {
-        console.log(data.results[i].title + ", created: " + data.results[i].date);
+        var box = document.createElement('div');
+
+        var titleOut = document.createElement('h3');
+        titleOut.textContent = data.results[i].title;
+        var dateOut = document.createElement('p');
+        dateOut.textContent = "Date: " + data.results[i].date;
+
+        var subjectOut = document.createElement('p');
+        var subjText = "Subjects: ";
+        for(var j = 0; j < data.results[i].subject.length; j++) {
+            subjText += data.results[i].subject[j];
+            if (j < data.results[i].subject.length - 1) {
+                subjText += ", ";
+            }
+        }
+        subjectOut.textContent = subjText;
+
+        var descOut = document.createElement('p');
+        descOut.textContent = "Description: " + data.results[i].description;
+
+        var readMoreButton = document.createElement('button');
+        readMoreButton.textContent = "Read More";
+        readMoreButton.addEventListener('click', function() {
+            location.href = data.results[i].url;
+        });
+
+        box.append(titleOut, dateOut, subjectOut, descOut, readMoreButton);
+        displayArea.append(box);
     }
 });
 
